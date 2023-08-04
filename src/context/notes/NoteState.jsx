f0eb9fc12b9c391d95241f585c980db6bfbe1db0
Himangshu Sarma <himangshu21_ug@ece.nits.ac.in>
2023-08-05 02:36:42 +0530

    Front End and basic functions created

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -2,23 +2,62 @@ import { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-    const data = {
-        "name": "Himangshu",
-        "branch": "ECE"
+    const NotesI = [
+        {
+          "_id": "64cc060f375c05d7c395f191",
+          "user": "64cbf7f811cddf36ee45bb55",
+          "title": "my title",
+          "description": "here is the description",
+          "tag": "deArr",
+          "date": "2023-08-03T19:54:55.849Z",
+          "__v": 0
+        },
+        {
+          "_id": "64cd4ebf23cdb5e747ea8cef",
+          "user": "64cbf7f811cddf36ee45bb55",
+          "title": "my title2",
+          "description": "here is the description2",
+          "tag": "deArr",
+          "date": "2023-08-04T19:17:19.372Z",
+          "__v": 0
+        },
+        {
+            "_id": "64cd4ebf23cdb5e747ea8cef",
+            "user": "64cbf7f811cddf36ee45bb55",
+            "title": "my title2",
+            "description": "here is the description2",
+            "tag": "deArr",
+            "date": "2023-08-04T19:17:19.372Z",
+            "__v": 0
+        }
+    ]
+    const [ notes, setNotes ] = useState(NotesI);
+
+    const addNote = (title, description, tag) => {
+      console.log("Added a new note");
+      const note = 
+        { 
+          "_id": "64cd4ebf23cdb5e747ea8cef",
+          "user": "64cbf7f811cddf36ee45bb55",
+          "title": title,
+          "description": description,
+          "tag": tag,
+          "date": "2023-08-04T19:17:19.372Z",
+          "__v": 0
+        }
+        setNotes(notes.concat(note));
     }
 
-    const [state, setState] = useState(data);
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name": "Sarma",
-                "branch": "CSE"            
-            })
-        }, 1000);
+    const deleteNote = () => {
+
+    }
+
+    const updateNote = () => {
+
     }
 
     return (
-        <NoteContext.Provider value = {{state, update}}>
+        <NoteContext.Provider value = {{notes, setNotes, addNote, deleteNote, updateNote}}>
             {props.children}
         </NoteContext.Provider>
     )
